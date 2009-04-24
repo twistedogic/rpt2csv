f04ccf88b51399325c139d81cabb768655473d8e
reillywatson <reillywatson@57131ed4-3083-11de-8e0c-53a27eea117e>
2009-04-24 04:00:44 +0000

    Don't reformat people's timestamp data

--- rpt2csv/Program.cs
+++ rpt2csv/Program.cs
@@ -8,13 +8,6 @@ namespace rpt2csv
 {
 	class Program
 	{
-		static double ConvertToUnixTimestamp( DateTime date )
-		{
-			DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-			TimeSpan diff = date - origin;
-			return Math.Floor(diff.TotalSeconds);
-		}
-
 		static void Main( string[] args )
 		{
 			if (args.Length != 2)
@@ -76,11 +69,6 @@ namespace rpt2csv
 											{
 												line = line + reader.ReadLine();
 											}
-										}
-										DateTime date;
-										if (DateTime.TryParse(str, out date))
-										{
-											str = ConvertToUnixTimestamp(date).ToString();
 										}
 										writer.Write(str);
 										if (i < columns.Length - 1)
