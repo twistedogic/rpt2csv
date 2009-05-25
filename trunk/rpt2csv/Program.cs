25f8f0c5194b0b7284522ec7128a7febed77b325
reillywatson <reillywatson@57131ed4-3083-11de-8e0c-53a27eea117e>
2009-05-25 17:01:42 +0000

    Properly escape fields so data with commas inside will work.  Use a default filename (foo.rpt -> foo.csv) if the second command-line argument is not specified.  Some code cleanup.

--- trunk/rpt2csv/Program.cs
+++ trunk/rpt2csv/Program.cs
@@ -10,34 +10,49 @@ namespace rpt2csv
 	{
 		static void Main( string[] args )
 		{
-			if (args.Length != 2)
+			if (args.Length < 1 || args.Length > 2)
 			{
 				System.Console.Out.WriteLine("Usage: rpt2csv [inputfile] [outputfile]");
 				return;
 			}
+			string inputPath = args[0];
+			string outputPath = String.Empty;
+			if (args.Length > 1)
+				outputPath = args[1];
+			else
+				outputPath = inputPath.Substring(0, inputPath.Length - 3) + ".csv";
+
+			ProcessRPT(inputPath, outputPath);
+
+			System.Console.Out.WriteLine("Success!");
+		}
+
+		private static void ProcessRPT(string inputPath, string outputPath)
+		{
 			try
 			{
-				using (FileStream fs = File.OpenRead(args[0]))
+				using (FileStream fs = File.OpenRead(inputPath))
 				{
 					using (StreamReader reader = new StreamReader(fs, Encoding.Default))
 					{
-						using (FileStream outfs = File.OpenWrite(args[1]))
+						using (FileStream outfs = File.OpenWrite(outputPath))
 						{
 							using (StreamWriter writer = new StreamWriter(outfs))
 							{
+								// The first line contains the column names; the second line is used to determine column length
 								string firstLine = reader.ReadLine();
 								string secondLine = reader.ReadLine();
 
 								string[] columns = secondLine.Split(' ');
 								List<string> columnNames = new List<string>();
+								
 								int charNo = 0;
-								int minLineLength = 0;
 								for (int i = 0; i < columns.Length; i++)
 								{
 									columnNames.Add(firstLine.Substring(charNo, Math.Min(columns[i].Length, firstLine.Length - charNo)).Trim());
 									charNo += columns[i].Length + 1;
 								}
-								minLineLength = charNo - columns[columns.Length - 1].Length + 2;
+								
 								for (int i = 0; i < columnNames.Count; i++)
 								{
 									writer.Write(columnNames[i]);
@@ -50,43 +65,51 @@ namespace rpt2csv
 								{
 									string line = reader.ReadLine();
 									lineNo++;
-									charNo = 0;
 									if (line.Trim().Length == 0)
 										break;
-
-									for (int i = 0; i < columns.Length; i++)
-									{
-										string str = string.Empty;
-										bool success = false;
-										while (!success)
-										{
-											try
-											{
-												str = line.Substring(charNo, Math.Min(columns[i].Length, line.Length - charNo)).Trim();
-												success = true;
-											}
-											catch
-											{
-												line = line + reader.ReadLine();
-											}
-										}
-										writer.Write(str);
-										if (i < columns.Length - 1)
-											writer.Write(",");
-										charNo += columns[i].Length + 1;
-									}
-									writer.WriteLine();
+									ProcessLine(line, reader, writer, columns);
 								}
 							}
 						}
 					}
 				}
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
 				System.Console.Out.WriteLine("An error has occurred in processing this .rpt.  Exception info: " + e.ToString());
 			}
-			System.Console.Out.WriteLine("Success!");
+		}
+
+		private static void ProcessLine( string line, StreamReader reader, StreamWriter writer, string[] columns )
+		{
+			int charNo = 0;
+			for (int i = 0; i < columns.Length; i++)
+			{
+				string str = string.Empty;
+				bool success = false;
+				while (!success)
+				{
+					try
+					{
+						str = line.Substring(charNo, Math.Min(columns[i].Length, line.Length - charNo)).Trim();
+						success = true;
+					}
+					catch
+					{
+						line = line + reader.ReadLine();
+					}
+				}
+
+				str = str.Replace("\"", "\"\"");
+				str = '"' + str + '"';
+				writer.Write(str);
+				if (i < columns.Length - 1)
+					writer.Write(",");
+				charNo += columns[i].Length + 1;
+			}
+			writer.WriteLine();
 		}
 	}
+
+
 }
