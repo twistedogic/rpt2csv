d8e24c4f8a9c09249007678c0a8d2451e0bf7c8d
reillywatson <reillywatson@57131ed4-3083-11de-8e0c-53a27eea117e>
2010-09-03 19:04:06 +0000

    Handle multiple sections in an RPT file (thanks pdmateus for the report and patch)

--- rpt2csv/Program.cs
+++ rpt2csv/Program.cs
@@ -51,34 +51,17 @@ namespace rpt2csv
 				{
 					using (StreamReader reader = new StreamReader(fs, Encoding.Default))
 					{
-						using (FileStream outfs = File.OpenWrite(outputPath))
+						int reportNo = 1;
+						while (!reader.EndOfStream)
 						{
-							using (StreamWriter writer = new StreamWriter(outfs))
-							{
-								// The first line contains the column names; the second line is used to determine column length
-								string firstLine = reader.ReadLine();
-								string secondLine = reader.ReadLine();
-
-								int[] columnLengths = secondLine.Split(' ').Select(s => s.Length).ToArray();
-								List<string> columnNames = new List<string>();
-
-								int charNo = 0;
-								foreach (int columnLength in columnLengths)
-								{
-									columnNames.Add(firstLine.Substring(charNo, Math.Min(columnLength, firstLine.Length - charNo)).Trim());
-									charNo += columnLength + 1;
-								}
-
-								WriteColumnNames(writer, columnNames);
-
-								while (!reader.EndOfStream)
-								{
-									string line = reader.ReadLine();
-									if (line.Trim().Length == 0)
-										break;
-									ProcessLine(line, reader, writer, columnLengths);
-								}
-							}
+							ProcessRPTSection(reader, outputPath, reportNo);
+							reportNo++;
+							// reports from multiple SELECTs have a blank line, a count of the rows affected, and then another
+							// blank line.  The first blank line is processed in ProcessRPTSection, so handle the other two here.
+							if (!reader.EndOfStream)
+								reader.ReadLine();
+							if (!reader.EndOfStream)
+								reader.ReadLine();
 						}
 					}
 				}
@@ -103,6 +86,42 @@ namespace rpt2csv
 			catch (Exception e)
 			{
 				System.Console.Out.WriteLine("An error has occurred in processing this .rpt.  Exception info: " + e.ToString());
+			}
+		}
+
+
+		private static void ProcessRPTSection(StreamReader reader, String outputPath, int reportNo)
+		{
+			if (reportNo > 1)
+				outputPath = outputPath.Replace(".csv", reportNo + ".csv");
+			using (FileStream outfs = File.OpenWrite(outputPath))
+			{
+				using (StreamWriter writer = new StreamWriter(outfs))
+				{
+					// The first line contains the column names; the second line is used to determine column length
+					string firstLine = reader.ReadLine();
+					string secondLine = reader.ReadLine();
+
+					int[] columnLengths = secondLine.Split(' ').Select(s => s.Length).ToArray();
+					List<string> columnNames = new List<string>();
+
+					int charNo = 0;
+					foreach (int columnLength in columnLengths)
+					{
+						columnNames.Add(firstLine.Substring(charNo, Math.Min(columnLength, firstLine.Length - charNo)).Trim());
+						charNo += columnLength + 1;
+					}
+
+					WriteColumnNames(writer, columnNames);
+
+					while (!reader.EndOfStream)
+					{
+						string line = reader.ReadLine();
+						if (line.Trim().Length == 0)
+							break;
+						ProcessLine(line, reader, writer, columnLengths);
+					}
+				}
 			}
 		}
 
